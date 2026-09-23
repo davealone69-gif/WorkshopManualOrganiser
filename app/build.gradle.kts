81df9cdf62c40b9e550ee6bb3c71db9145676592
davealone69-gif <davealone69@gmail.com>
2026-09-23 09:29:14 +0800

    chore: remove unused documentfile dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.documentfile)
 
     implementation(libs.coil.compose)
     implementation(libs.okhttp)
